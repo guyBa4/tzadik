be877b0a7891cf9ce40a818834a2dc27b14aa460
guy <barak4@post.bgu.ac.il>
2023-11-14 14:30:50 +0200

    hopefully without credentials

--- dal/DB.js
+++ dal/DB.js
@@ -1,19 +1,8 @@
 const Sequelize = require('sequelize');
-// const credentials = require('./Credentials')
 
-const sequelize = new Sequelize({
-  database: 'postgres',
-  username: 'postgres',
-  password: 'tzdbnig228',
-  host: 'tzadik.csidgvwyscy4.us-east-2.rds.amazonaws.com',
-  port: '5432',
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
-});
+
+const credentials = require('./credentials');
+
+const sequelize = new Sequelize(credentials);
 
 module.exports = sequelize;
